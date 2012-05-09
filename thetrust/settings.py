cc77a18722fec4436418247544426eebd19934f6
Nathan Fair <nathan.fair@gmail.com>
2012-05-09 00:05:50 -0700

    update to setting to include a database name, user, and password

--- thetrust/settings.py
+++ thetrust/settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2'
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': 'Users',                      # Or path to database file if using sqlite3.
+        'USER': 'Admin',                      # Not used with sqlite3.
+        'PASSWORD': '8something',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
