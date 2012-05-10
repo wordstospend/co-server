d058799d4a3fa7d6ae92c9d46e72ffdefbfa1784
Nathan Fair <nathan.fair@gmail.com>
2012-05-09 22:09:17 -0700

    added the new app testtem

---
+++ testthem/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+import json
+
+def track(request, id):
+    my_data= [{'person': 12345}, {"locations":[[17, 18],[12,14]]}]
+    response =  HttpResponse(my_data, content_type='application/json')
+    return response
