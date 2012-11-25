04ae886728f3ace1012b0a061351f55ab4f7f680
Nathan Fair <nfair@saymedia.com>
2012-11-24 22:31:47 -0800

    mock data added

--- testthem/views.py
+++ testthem/views.py
@@ -3,6 +3,6 @@ import json
 
 def track(request):
     print "got this far"
-    my_data= [{'person': 12345}, {"locations":[[17, 18],[12,14]]}]
+    my_data= [{'person': 12345}, {"locations":[[-122.4496179,37.7657324],[-122.4236498,37.7239323]]}]
     response =  HttpResponse(my_data, content_type='application/json')
     return response
