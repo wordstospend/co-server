ca4b4c1cd886e4a1f67ac9b67df499061c4306d2
Nathan Fair <nfair@saymedia.com>
2012-11-25 17:07:13 -0800

    the output of /trustme endpoint is now {"locations":[time,[longitude, latitude]]}

--- testthem/views.py
+++ testthem/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 import json
+import datetime
 
 def track(request):
     print "got this far"
-    my_data= [{'person': 12345}, {"locations":[[-122.4496179,37.7657324],[-122.4236498,37.7239323]]}]
+    t = datetime.datetime.now()
+    my_data= {"locations":[time.mktime(t.timetuple()), [-122.4496179,37.7657324]]}
+    # [1353826644.0,[-122.4236498,37.7239323]]]}
     response =  HttpResponse(my_data, content_type='application/json')
     return response
